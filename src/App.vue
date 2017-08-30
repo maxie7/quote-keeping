bf556b0970249d58f40040c8f29f40b3b93c4b90
Kos Max <maxie7@protonmail.com>
2017-08-30 14:16:18 +0200

    Add components QuoteGrid & Quote

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,12 @@
 
 <script>
     export default {
-        
+        data: function() {
+          return {
+            quotes: ['Just a Quote to see something!'],
+            maxQuotes: 10
+          }
+        }
     }
 </script>
 
